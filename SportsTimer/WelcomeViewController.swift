d045dbbd883da3a5cdb12bef92e799448e8c2e59
calebrudnicki <calebrudnicki@gmail.com>
2017-05-13 15:00:23 -0400

    Improved the onboarding / settings page

--- SportsTimer/WelcomeViewController.swift
+++ SportsTimer/WelcomeViewController.swift
@@ -13,9 +13,15 @@ class WelcomeViewController: UIViewController {
     @IBOutlet weak var player1TextField: UITextField!
     @IBOutlet weak var player2TextField: UITextField!
     @IBOutlet weak var sportsSegmentedController: UISegmentedControl!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagesListArray = [#imageLiteral(resourceName: "SoccerBackground"), #imageLiteral(resourceName: "BasketballBackground"), #imageLiteral(resourceName: "BaseballBackground"), #imageLiteral(resourceName: "HockeyBackground"), #imageLiteral(resourceName: "FootballBackground")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.imageView.animationImages = imagesListArray
+        self.imageView.animationDuration = 10
+        self.imageView.startAnimating()
     }
 
     override func didReceiveMemoryWarning() {
